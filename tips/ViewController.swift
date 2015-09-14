def1f82e208e29d5ae21551d735226ea02b6aa48
Jason Fain <jasonfain@somadigital.com>
2015-09-14 16:51:29 -0700

    WIP at 4:51pm

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -13,13 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalAmount.text = "$0.00"
     }
 
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var totalAmount: UILabel!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var billAmount = NSString(string: billField.text!).doubleValue
+        var tip = billAmount * 0.2
+        var total = billAmount + tip
+        tipLabel.text = "$\(tip)"
+        totalAmount.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
